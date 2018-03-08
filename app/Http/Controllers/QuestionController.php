b42d95c354654926d1d1afefa77bd341afb2ca78
theaeiou <theaeiou305@hotmail.com>
2018-03-08 18:47:17 +0700

    fixed

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class QuestionController extends Controller
 {
     public function index(){
-      $title = 'Welcome to Laravel';
+      $title = 'สวัสดี';
       //return view('pages.index', compact('title'));
       return view('pages.index')->with('title', $title);
     }
@@ -19,7 +19,7 @@ class QuestionController extends Controller
     public function duty(){
       $data = array('title' => 'Duty',
                     'duties' => ['Programing'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu sodales nunc. Morbi quis placerat augue.',
-                    'Design'=>'consectetur adipiscing elit. Nunc nibh tellus, tincidunt sit amet tincidunt sit amet.', 
+                    'Design'=>'consectetur adipiscing elit. Nunc nibh tellus, tincidunt sit amet tincidunt sit amet.',
                     'Organize'=>'euismod in neque. Vestibulum sollicitudin at urna nec bibendum. Duis nec lectus vel tortor euismod ullamcorper.']
 
     );
